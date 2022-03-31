a8e74328b7346e0bc14b0061567d4a9dfc395004
hanzalarohan123 <hanzalarohan@gmail.com>
2022-04-01 03:16:00 +0500

    added api axios service

--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -3,24 +3,27 @@ import React, { Component } from 'react'
 //wow
 
 class PostList extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         post:[]
-      }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      post: []
     }
-    componentDidMount()
-    {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(result =>this.setState={post:result.data}).catch(error =>{console.log(error)})
-    }
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(reslt => { this.setState({ post: reslt.data }) }).catch(error => { console.log(error) })
+  }
   render() {
-    const{post} = this.state
+    const { post } = this.state
     return (
       <div>
+
         {post.length ?
-        post.map(pos=><div key={pos.id}>{pos.title}</div>):null}
+        post.map(pos => <div key={pos.id} > {pos.title} </div>):null}
+      
       </div>
+
+
     )
   }
 }
